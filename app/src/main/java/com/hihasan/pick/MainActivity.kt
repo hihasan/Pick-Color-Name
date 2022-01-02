64811a318e1b494e3a02e1897634f2b56a55292b
hihasan07 <hihasan07@gmail.com>
2022-01-02 15:05:05 +0600

    issue fixed

--- app/src/main/java/com/hihasan/pick/MainActivity.kt
+++ app/src/main/java/com/hihasan/pick/MainActivity.kt
@@ -1,13 +1,18 @@
-package com.hihasan.pickcolorname
+package com.hihasan.pick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.hihasan.pick.databinding.ActivityTestBinding
+import com.hihasan.pickcolorname.ColorUtils
+import com.hihasan.pick.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_test)
 
         Log.d(MainActivity::class.java.simpleName, ColorUtils.getColorNameFromColor("#DDDDDD"))
     }
